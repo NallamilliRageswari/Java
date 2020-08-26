88bb0996506b4b749a612460c1d4b55e2d7482a6
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-08-26 19:01:41 +0530

    Add files via upload

---
+++ Overload_Constructor.java
@@ -0,0 +1,44 @@
+//Overload constructor
+
+package Pack1;
+class Bank
+{
+	String name;
+	int acc_no;
+	Bank()
+	{
+		name = "suresh";
+		acc_no = 10;
+	}
+	Bank(String name)
+	{
+		this.name = name;
+		acc_no = 11;
+	}
+	Bank(String name , int acc_no)
+	{
+		this.name = name;
+		this.acc_no = acc_no;
+	}
+	void showBank()
+	{
+		System.out.println("name = "+name+"\nacc_no ="+acc_no);
+	}
+}
+public class Overload_Constructor 
+{
+	public static void main(String[] args) 
+	{
+		Bank b1 = new Bank();
+		b1.showBank();
+		Bank b2 = new Bank("naveen");
+		b2.showBank();
+		Bank b3 = new Bank("sai",12);
+		b3.showBank();
+	}
+}
+
+
+	
+
+
