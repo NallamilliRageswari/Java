eef3068f98210597ed4375a7cc06a680941dcc85
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-01 20:43:18 +0530

    Add files via upload

---
+++ Cone_Volume.java
@@ -0,0 +1,15 @@
+package Formulas;
+import java.util.*;
+public class Cone_Volume
+{
+	public static void main(String[] args) 
+	{
+	Scanner obj = new Scanner(System.in);
+	System.out.println("enter radius:");
+	int r = obj.nextInt();
+	System.out.println("enter height:");
+	int h = obj.nextInt();	
+	double v = 3.14 * (r*r)*h / 3;
+    System.out.println("Volume of cone = "+ v);
+	}
+}
