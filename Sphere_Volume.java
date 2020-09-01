688fbf9d673ce9d28f0957e15f93c20ba803e9f9
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-01 20:42:12 +0530

    Add files via upload

---
+++ Sphere_Volume.java
@@ -0,0 +1,14 @@
+package Formulas;
+import java.util.*;
+public class Sphere_Volume
+{
+	public static void main(String[] args) 
+	{
+	 Scanner obj = new Scanner(System.in);
+	 System.out.println("enter radius:");
+	 int r = obj.nextInt();
+	 double v = (4.0/3.0)*3.14*(r*r*r);
+	 System.out.println("Volume of sphere = "+v);
+	}
+
+}
