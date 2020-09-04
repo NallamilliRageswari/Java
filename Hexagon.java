5fc9f9d7204b50a491699a85c3d5e955bc05e7c6
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-04 17:48:25 +0530

    Add files via upload

---
+++ Hexagon.java
@@ -0,0 +1,13 @@
+package Formulas;
+import java.util.*;
+public class Hexagon
+{
+	public static void main(String[] args) 
+	{
+	Scanner obj = new Scanner(System.in);
+	System.out.println("enter length of a side ");
+	double s= obj.nextDouble();
+	double a = (3*Math.sqrt(3)*(s*s))/2;
+	System.out.println("Area of Hexagon = "+a);
+	}
+}
