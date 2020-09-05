31b9fca2cffa5778f105a1f909da2189089fa444
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-05 21:18:58 +0530

    Add files via upload

---
+++ Heptagon.java
@@ -0,0 +1,13 @@
+package Formulas;
+import java.util.*;
+public class Heptagon
+{
+	public static void main(String[] args) 
+	{
+	  Scanner obj = new Scanner(System.in);
+      System.out.println("enter length of a side: ");
+      float s = obj.nextFloat();
+      float a = 3.634f * s * s;
+      System.out.println("Area of Heptagon = "+ a);
+	}
+}
