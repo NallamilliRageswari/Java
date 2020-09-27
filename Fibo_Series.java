5fe314336df7bb454ccf3fbc47cd5fb934a05275
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-27 19:46:34 +0530

    Add files via upload

---
+++ Fibo_Series.java
@@ -0,0 +1,22 @@
+package pgm3;
+import java.util.*;
+public class Fibo_Series
+{
+	public static void main(String[] args) 
+	{
+	Scanner obj = new Scanner(System.in);
+	System.out.println("enter number :");
+	int n = obj.nextInt(); 
+	int t1 = 0 , t2 = 1;
+	System.out.println("Fibonacci numbers upto given number:"+n);
+	System.out.print(t1);  
+	int sum=0;
+	for(int i = 2; sum<n; i++) 
+	{	
+		sum = t1 + t2; 
+		t1 = t2;  
+		t2 = sum;  
+		System.out.print(" "+t1);  
+	}
+    } 
+}
