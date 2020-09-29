5f39d9276f6e515b3e20eb3cef959234499e7442
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-09-29 21:01:28 +0530

    Add files via upload

---
+++ Ugly_Number.java
@@ -0,0 +1,30 @@
+package pgm2;
+import java.util.*;
+public class Ugly_Number
+{
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter number :");
+	int n = sc.nextInt(); 
+	int x=0; // ugly number
+	 
+	while(n != 1) 
+	{
+		if(n%5==0)  
+         		n = n/5;			
+		else if(n%3==0)
+		n=n/3 ; 
+		else if(n%2==0)
+		n = n/2;
+		else
+		{ 
+		x= 1;
+		break;
+		}
+	}
+	 if(x==0)
+		 System.out.println("Ugly Number");
+	 else
+		 System.out.println("Not Ugly Number");
+	} 	
+}
