7c53028ea867467023a6e61aee1c0058eec6b2d6
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-11-05 17:46:59 +0530

    Add files via upload

---
+++ Pattern14.java
@@ -0,0 +1,25 @@
+/*
+Output Pattern:
+				A
+				AB
+				ABC
+				ABCD	
+				ABCDE
+ */
+package Patterns;
+import java.util.*;
+public class Pattern14
+{
+	public static void main(String[] args) {
+		char i;
+		char j;
+		for( i ='A'; i<='E';i++) //5 rows
+		{
+			for( j='A'; j<=i;j++) // cols
+			{
+				System.out.print(j); //A
+			}
+			System.out.println();
+		}
+	} 
+}
