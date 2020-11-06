319437c5787481f2572943b71a51774981f3b586
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-11-06 19:06:43 +0530

    Add files via upload

---
+++ Pattern15.java
@@ -0,0 +1,23 @@
+/*
+Output Pattern:
+				*****
+				****
+				***
+				**
+				*
+*/
+package Patterns;
+import java.util.*;
+public class Pattern15
+{
+	public static void main(String[] args) {
+		for(int i =1; i<=5;i++) // 5 rows, i=2
+		{
+			for(int j=5; j>=i;j--) // 5 cols
+			{
+				System.out.print("*"); //*****
+			}
+			System.out.println();
+		}
+	}
+}
