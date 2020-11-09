2c6c279667779395982edd77413260a1c738a0af
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-11-09 18:16:40 +0530

    Add files via upload

---
+++ Pattern22.java
@@ -0,0 +1,23 @@
+/*
+Output Pattern:
+				E E E E E 
+				D D D D 
+				C C C 
+				B B 
+				A 
+ */
+package Patterns1;
+import java.util.*;
+public class Pattern22
+{
+	public static void main(String[] args) {
+		for(int i='E'; i>='A';i--) // 5 rows
+		{
+			for(int j= 'A';j<=i; j++) // 5,4 cols
+			{
+				System.out.print((char)i+" "); 
+			}
+			System.out.println(); //nextline
+		}
+	}
+}
