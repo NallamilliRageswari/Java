a85edf5f434e3aa5cc1b9724a5f31628bf786bc9
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-11-12 17:27:39 +0530

    Add files via upload

---
+++ Pattern27_1.java
@@ -0,0 +1,27 @@
+/*
+Output Pattern:
+				A
+   			   BB
+  			  CCC
+ 			 DDDD
+		    EEEEE
+ */
+package Patterns1;
+import java.util.*;
+public class Pattern27_1
+{
+	public static void main(String[] args) {
+		int n =5;
+		
+		for(int i=1; i<=5; i++) // 5 rows
+		{
+			for(int j=n-1; j>=i; j--) // 4 spaces
+				System.out.print(" ");
+			
+			for(int k= 1; k<=i; k++) // repeats
+				System.out.print((char)(i+64)); // A
+			
+			System.out.println();
+		}
+	}
+}
