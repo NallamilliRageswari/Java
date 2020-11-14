48717eddf05d36f616e1f2ee138da29bb4fd673a
NallamilliRageswari <69524473+NallamilliRageswari@users.noreply.github.com>
2020-11-14 20:54:54 +0530

    Add files via upload

---
+++ Pattern30.java
@@ -0,0 +1,27 @@
+/*
+ Output Pattern:
+ 					55555
+			  	     4444
+			          333
+			           22
+			            1
+
+ */
+package Patterns1;
+public class Pattern30
+{
+	public static void main(String[] args) {
+		int n = 5; // 5 rows
+		
+		for(int i=n; i >= 1 ; i--)// i = 5, 4,3 rows
+		{
+			for(int j= n-1; j >= i ; j--)// j=4 >= 3	
+				System.out.print(" "); // 0,1,2 -> spaces
+			
+			for(int k=1; k <= i; k++) // k=1 <= 3 ->t 
+				System.out.print(i); // 5,4,3  -> *
+			
+			System.out.println();			
+		}
+} 
+}
